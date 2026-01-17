c5b700ab13f51d27d934c0b0e3a9a8f30cf0f76b
Abdullah Bawazeer <145490935+abawzr@users.noreply.github.com>
2026-01-17 07:00:13 +0300

    modified player + added enemy + added padlock puzzle

--- Assets/Rafa/Scripts/PlayerMovement.cs
+++ Assets/Rafa/Scripts/PlayerMovement.cs
@@ -4,76 +4,84 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Move")]
-    public CharacterController cc;
-    public float speed = 6.0f;
+    [SerializeField] private float speed = 6.0f;
 
     [Header("Jump")]
-    public float gravity = -9.81f;
-    public float SinglejumpHeight = 2f;
-    //public float DoublejumpHeight = 4f;
-    //public int maxJumpCount = 2;
-    //int jumpCount = 0;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float SinglejumpHeight = 4f;
 
     [Header("Look")]
-    public float mouseSensitivity = 150f;
-    public float lookLimit=80f;
-    public Transform cameraHolder;
+    [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private float lookLimit = 80f;
+    [SerializeField] private Transform cameraHolder;
 
-    bool isGrounded;
-    Vector3 velocity;
-    float xRotation = 0f;
-    public bool controlsEnabled=true;
-    void Awake() {
+    private CharacterController cc;
+    private float velocity;
+    private float xRotation = 0f;
+
+    public bool controlsEnabled = true;
+
+    private void Awake()
+    {
         cc = GetComponent<CharacterController>();
-        if (!cameraHolder && Camera.main) cameraHolder = Camera.main.transform;
     }
-     void Update()
+
+    private void Update()
+    {
+        if (!controlsEnabled) return;
+
+        Move();
+        Jump();
+        ApplyGravity();
+    }
+
+    private void LateUpdate()
     {
         if (!controlsEnabled) return;
 
         Look();
-        Move();
-        Jump();
     }
-     void Look()
+
+    private void Look()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        xRotation = Mathf.Clamp(xRotation, -lookLimit, lookLimit);
         cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        transform.Rotate(Vector3.up * mouseX);
+        transform.Rotate(transform.up * mouseX);
     }
-    void Move()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        move=Vector3.ClampMagnitude(move,1f);
-        cc.Move(move * speed * Time.deltaTime);
-    }
-    void Jump()
-    {
-        if(cc.isGrounded&&velocity.y<0f)
-            velocity.y=-2f;
 
-        //Single Jump
-        if(cc.isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            velocity.y = Mathf.Sqrt(SinglejumpHeight * -2f * gravity);
-           //jumpCount++;
-        }
-        //Double Jump
-        /*else if(!cc.isGrounded && Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
-        {
-            velocity.y = Mathf.Sqrt(DoublejumpHeight*-2f*gravity);
-            jumpCount++;
-            
-        }*/
-        velocity.y += gravity * Time.deltaTime;
-        cc.Move(Vector3.up* velocity.y * Time.deltaTime);
+    private void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        move = Vector3.ClampMagnitude(move, 1f) * speed;
+        move.y = velocity;
+        cc.Move(move * Time.deltaTime);
     }
-    
+
+    private void Jump()
+    {
+        //Single Jump
+        if (cc.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity = SinglejumpHeight;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (cc.isGrounded && velocity < 0f)
+            velocity = -2f;
+
+        else
+        {
+            velocity += gravity * Time.deltaTime;
+        }
+    }
+
 }
