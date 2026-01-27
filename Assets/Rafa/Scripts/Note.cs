84148c932c900ff40223193c5b02b430e42c9e6c
Abdullah Bawazeer <145490935+abawzr@users.noreply.github.com>
2026-01-27 19:31:42 +0300

    add note script

---
+++ Assets/Rafa/Scripts/Note.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour, IInteractable
+{
+    public bool CanInteract { get; set; }
+
+    public void Interact(PlayerInventory playerInventory)
+    {
+        
+    }
+}
