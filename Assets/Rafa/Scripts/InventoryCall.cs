2bea9c8768b04bd232172aed9dfe6586c3c61e94
Rafa Balkhdhar <rafa.o5dr@gmail.com>
2026-01-28 16:46:27 +0300

    START SCENE , Notes & inventory Call

---
+++ Assets/Rafa/Scripts/InventoryCall.cs
@@ -0,0 +1,57 @@
+using System.Runtime.Serialization;
+using UnityEngine;
+using System.Collections;
+
+public class InventoryCall : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject inventoryUI;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float claoseAnimationTime = 0.2f;
+    private bool _isOpen;
+    void Start()
+    {
+        _isOpen = false;
+        if (inventoryUI != null)
+            inventoryUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventory();
+        }
+    }
+
+    private void ToggleInventory()
+    {
+        _isOpen = !_isOpen;
+        if (_isOpen)
+            Open();
+        else
+            Close();
+    }
+    private void Open()
+    {
+        if (!inventoryUI) return;
+        inventoryUI.SetActive(true);
+        // if (animator != null) animator.SetBool("isOpen", true);
+    }
+    private void Close()
+    {
+        if (!inventoryUI) return;
+        inventoryUI.SetActive(false);
+        // if (animator != null) animator.SetBool("isClose", false);
+        // StartCoroutine(DisableAfterClose());
+
+
+    }
+    // private IEnumerator DisableAfterClose()
+    // {
+    //     yield return new WaitForSeconds(claoseAnimationTime);
+    //     if (!_isOpen && inventoryUI)
+    //         inventoryUI.SetActive(false);
+    // }
+}
--- Assets/Rafa/Scripts/Note.cs
+++ Assets/Rafa/Scripts/Note.cs
@@ -1,11 +1,63 @@
 using UnityEngine;
+using TMPro;
 
 public class Note : MonoBehaviour, IInteractable
 {
     public bool CanInteract { get; set; }
 
+    [SerializeField] private string noteText; // text on the note
+
+    [SerializeField] private GameObject noteUI; // UI to display the note
+    [SerializeField] private TMP_Text noteTextTMP;
+    bool _isReading = false;
+    private void Awake()
+    {
+        CanInteract = true;
+
+
+        if (noteUI)
+            noteUI.SetActive(false);
+    }
     public void Interact(PlayerInventory playerInventory)
     {
-        
+        // pick up note to read on E key press drop it on E key press 
+
+        if (_isReading)
+        {
+            CloseNote();
+        }
+        else
+        {
+            OpenNote();
+
+        }
+    }
+    private void OpenNote()
+    {
+        _isReading = true;
+
+        if (noteTextTMP != null)
+        {
+            noteTextTMP.text = noteText;
+        }
+
+        if (noteUI != null)
+        {
+            noteUI.SetActive(true);
+        }
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    private void CloseNote()
+    {
+        _isReading = false;
+
+        if (noteUI != null)
+        {
+            noteUI.SetActive(false);
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
